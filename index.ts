206768ffbf619e03506da338c8d0cfeb1e1bbc3e
nwabimady <nwabimady@gmail.com>
2023-07-20 23:50:37 +0200

    Functions: 2

--- index.ts
+++ index.ts
@@ -1,13 +1,32 @@
-function sum1(a: number, b: number) {
-    const result = a + b;
-    console.log(result);
-    return result
+interface Rectangular {
+    width: number;
+    height: number;
+    
 }
 
-const sum2 = function (a, b) {
-    console.log(a, b);
+class Column implements Rectangular{
+    width: number;
+    height: number;
+
+    undefined() {
+        return this.width * this.height;
+    }
 }
 
-const sum3 = (a, b) => {
-    console.log(a + b);
+function sum(a, b){
+    return a + b;
 }
+
+function add(a: string, b:number): number;
+function add(a: string, b:string): string;
+function add(a: number, b:number): number;
+function add(a:any, b: any) {
+    if(typeof a === 'string' && typeof b === "number") {
+        const aNumber = parseFloat(a);
+        return aNumber + b;
+    }
+
+    return a + b;
+}
+
+add("1", 2);
