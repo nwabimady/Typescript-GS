bd11ded7325eace00ea12cca887ec8c95def0777
nwabimady <nwabimady@gmail.com>
2023-07-21 00:40:18 +0200

    Type Assertions: interface

--- index.ts
+++ index.ts
@@ -1,21 +1,18 @@
+interface Vector2D {
+    x: number;
+    y: number;
+}
+
 class Column {
-    material?:string;
-    dimensions?: {
-        x: number;
-        y: number;
-    }
+    material: string;
+    dimensions: Vector2D;
+
+    constructor (material: string, dimensions: Vector2D) {
+        this.material = material;
+        this.dimensions = dimensions;
+    } 
 }
 
-const column = new Column();
-column.material = 'Concrete';
-setupDimenions(column);
+const column = new Column('Concrete', {x: 30, y:40} );
 
-console.log(column.dimensions!.x);
-
-
-function setupDimenions(column: Column) {
-    column.dimensions = {
-        x:30,
-        y:40
-    }
-}
+console.log(column.dimensions.x);
