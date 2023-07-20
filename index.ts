32826b97e8d4d4fcd0ec5fc9f69df2cec6ff8023
nwabimady <nwabimady@gmail.com>
2023-07-21 00:25:52 +0200

    Type Assertions: 2

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
-const message = document.getElementById("greet-message") as HTMLDivElement;
-
-message.onclose = () => {
-    console.log('The greeter has closed');
+class Column {
+    material?:string;
+    dimensions?: {
+        x: number;
+        y: number;
+    }
 }
+
+const column = new Column();
+column.material = 'Concrete';
+column.dimensions = {
+    X: 30,
+    y: 40
+};
+
+console.log(column.dimensions.x);
