c3322c7f9c412df877ed2498714396f6138e9a18
nwabimady <nwabimady@gmail.com>
2023-07-21 00:28:19 +0200

    Type Assertions: Longer undefined

--- index.ts
+++ index.ts
@@ -10,8 +10,10 @@ const column = new Column();
 column.material = 'Concrete';
 setupDimenions(column);
 
-console.log(column.dimensions?.x);
 
+if(column.dimensions !== undefined) {
+console.log(column.dimensions.x);
+}
 
 function setupDimenions(column: Column) {
     column.dimensions = {
