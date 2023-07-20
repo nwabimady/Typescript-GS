a2bcb0261ffdd37e9d59f53cf928ad3e248202b5
nwabimady <nwabimady@gmail.com>
2023-07-21 00:01:27 +0200

    Enums

--- index.ts
+++ index.ts
@@ -1,32 +1,15 @@
-interface Rectangular {
-    width: number;
-    height: number;
-    
+enum Material {
+    CONCRETE = "CONCRETE",
+    WOOD = "WOOD",
+    BRICK = "BRICK",
+    STEEL = "STEEL",
 }
+class Column {
+    material: Material;
 
-class Column implements Rectangular{
-    width: number;
-    height: number;
-
-    undefined() {
-        return this.width * this.height;
+    constructor(material: Material) {
+        this.material = material;
     }
 }
 
-function sum(a, b){
-    return a + b;
-}
-
-function add(a: string, b:number): number;
-function add(a: string, b:string): string;
-function add(a: number, b:number): number;
-function add(a:any, b: any) {
-    if(typeof a === 'string' && typeof b === "number") {
-        const aNumber = parseFloat(a);
-        return aNumber + b;
-    }
-
-    return a + b;
-}
-
-add("1", 2);
+const column = new Column(Material["BRICK"]);
