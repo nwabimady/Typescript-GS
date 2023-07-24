e4141be4f313df70f1faaefef7d45271a93a4c54
nwabimady <nwabimady@gmail.com>
2023-07-25 00:54:41 +0200

    Keyof

--- index.ts
+++ index.ts
@@ -1,80 +1,28 @@
-function identity<Type>(arg: Type): Type {
-    return arg;
-  }
-  
-  const value = identity<string>("myString");
+interface Column {
+    material: string;
+    width: number;
+    depth: number;
+    height: number;
+}
 
-  const value = identity("myString");
+const columnPropertyName: keyof Column = "depth";
 
-  const stringValue:string = 'myString'
-const value = identity(stringValue);
+class ConcreteColumn implements Column {
+    material: "Concrete";
+    width: number;
+    depth: number;
+    height: number;
 
-
-//Narrowing Generics
-function logLength<Type>(arg: Type): Type {
-    console.log(arg.length);
-    return arg;
-  }
-
-  function logLength<Type>(arg: Type): Type {
-    if (Array.isArray(arg)) {
-      console.log(arg.length);
+    constructor(width = 30, depth = 30, height = 3000) {
+        this.width = width;
+        this.depth = depth;
+        this.height = height;
     }
-    return arg;
-  }
 
-  //Generic Types
-  interface Car<Type> {
-    license: Type;
-  }
+    LogData(data: keyof Column) {
+        console.log(this[data]);
+    }
+}
 
-const car: Car<string> = { license: "license" };
-const car: Car<string> = { license: 3 };
-
-class Car<Type> {
-    license: Type;
-    getLicense:()=>Type;
-  }
-
-  const car = new Car<string>();
-car.license = "license";
-const carLicense = car.getLicense();
-//carLicense type is string
-
-
-// Generic Constraints
-interface WithLength {
-    length: number;
-  }
-
-  function logLength<Type extends WithLength>(arg: Type) {
-    console.log(arg.length);
-  }
-
-  logLength<number>(3);
-
-  type SubType<Type extends ExtendedType, ExtendedType> = Type;
-
-  let validSubType: SubType<string, WithLength>;
-let validSubType: SubType<number, WithLength>;
-
-
-//Union Types and Generics
-type WithLength<Type> = Type & { length: number };
-
-interface CustomType {
-    attribute: string;
-  }
-  
-  const customTypeWithLength: WithLength<CustomType> = {
-    attribute: "myString",
-    length: 3,
-  };
-
-
-  //Optional Generics
-  type WithLengthDefaultNumber<Type = CustomType> = Type & { length: number };
-const customTypeWithLength: WithLengthDefaultNumber = {
-  attribute: "myString",
-  length: 3,
-};
+const column = new ConcreteColumn();
+column.LogData("depth");
