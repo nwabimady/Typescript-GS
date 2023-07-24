00dba15350ee48b762511417ee027bfd202c5cac
nwabimady <nwabimady@gmail.com>
2023-07-25 01:03:16 +0200

    Typeof

--- index.ts
+++ index.ts
@@ -1,28 +1,9 @@
-interface Column {
-    material: string;
-    width: number;
-    depth: number;
-    height: number;
+function computeArea(width: number, length: number) {
+    return width * length;
 }
 
-const columnPropertyName: keyof Column = "depth";
-
-class ConcreteColumn implements Column {
-    material: "Concrete";
-    width: number;
-    depth: number;
-    height: number;
-
-    constructor(width = 30, depth = 30, height = 3000) {
-        this.width = width;
-        this.depth = depth;
-        this.height = height;
-    }
-
-    LogData(data: keyof Column) {
-        console.log(this[data]);
-    }
+function invokeDelegate(callback: typeof computeArea) {
+    return callback(5, 10); // call the function with arguments (5
 }
 
-const column = new ConcreteColumn();
-column.LogData("depth");
+invokeDelegate(computeArea);
