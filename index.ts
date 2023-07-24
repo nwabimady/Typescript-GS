9d44e37cc1adcd38ba7e695d72f770dd4fcf1305
nwabimady <nwabimady@gmail.com>
2023-07-25 00:12:24 +0200

    Literal Types

--- index.ts
+++ index.ts
@@ -1,30 +1,12 @@
-interface Logeable {
-    Log: () => void;
-    
+const CountryList = ["South Africa", "Germany", "Spain"] as const;
+type Country = typeof CountryList[number];
+
+function isCountry(text: string): text is Country {
+    return CountryList.includes(text as any);
 }
 
-class Column {
-    rebarWeight: number;
-    Log() {
-        console.log(this.rebarWeight);
-    };
-    
-}
-
-class GlassWindow {
-    frameCount: number;
-    Log() {
-        console.log(this.frameCount);
+function registerUser(name: string, country: string) {
+    if(isCountry(country)) {
+        country
     }
 }
-
-function LogItems(items: Logeable[]) {
-    for(const item of items) {
-        item.Log();
-    }
-}
-
-const column = new Column();
-const glassWindow = new GlassWindow();
-const allItems = [column, glassWindow];
-LogItems(allItems);
