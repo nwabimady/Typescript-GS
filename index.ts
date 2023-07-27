b052b00edda4eaa577b17bb94d1a8b6f1ca804aa
nwabimady <nwabimady@gmail.com>
2023-07-27 23:23:07 +0200

    Conditional Types: Never concept

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
-type Flatten<Type> = Type extends Array<infer Item> ?  Item : Type;
-
-const firstArray = ["a", "b", "c"];
-let flattenArray: Flatten<typeof firstArray>;
-
-const secondArray = "fgh";
-let flattenArray2: Flatten<typeof secondArray>;
+type TextArray<Type> = Type extends string ? Type[] : never;
