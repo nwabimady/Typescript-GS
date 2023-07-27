c56945c9d6d7d56790e4363b8d0dc1df9ce196dd
nwabimady <nwabimady@gmail.com>
2023-07-27 23:21:35 +0200

    Conditional Type: 2: Infer

--- index.ts
+++ index.ts
@@ -1,29 +1,7 @@
-type Structural = {
-    loads: number [],
-}
+type Flatten<Type> = Type extends Array<infer Item> ?  Item : Type;
 
-type NonStructural = {
-    thermalInsulation: number;
-}
+const firstArray = ["a", "b", "c"];
+let flattenArray: Flatten<typeof firstArray>;
 
-type BaseGroup<Type> = {
-    items: Type[];
-}
-
-type IsStructural<Type> = Type extends Structural 
-    ? { getAllLoads: () => number[]} 
-    : { getTotalInsulation: () => number };
-
-type ItemGroup<Type> = BaseGroup<Type> & IsStructural<Type>;
-
-const group: ItemGroup<Structural> = {
-    items: [],
-
-    getAllLoads() {
-        const allLoads: number[] = [];
-        for(const item of this.items) {
-            allLoads.push(...item.loads);
-        }
-        return allLoads;
-    }
-}
+const secondArray = "fgh";
+let flattenArray2: Flatten<typeof secondArray>;
